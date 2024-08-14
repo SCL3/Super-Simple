59c6e98eea3887d0227f80b7f5a22283242dfa83
SCL3 <106513368+SCL3@users.noreply.github.com>
2024-08-14 21:17:06 +0200

    Add files via upload

---
+++ Super Simple/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"controller"},{"l":"items"},{"l":"model"},{"l":"scene"},{"l":"utils"}];updateSearchResults();
